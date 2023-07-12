f40c6a54b9470764a3b076793d5c433bfb835874
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-07-12 00:14:23 -0300

    Project created

---
+++ src/main/java/com/listadegames/gamesapp/GamesappApplication.java
@@ -0,0 +1,13 @@
+package com.listadegames.gamesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listadegames/gamesapp/GamesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listadegames.gamesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
